aa1b681e92ae7d0c57f3a61e8d125b9e4893fe9f
fevzierenn <fevzierencicek@gmail.com>
2026-09-08 01:15:15 +0300

    Group Anagrams 004 - fixed

--- src/Solutions/N150/GroupAnagrams_004/main.java
+++ src/Solutions/N150/GroupAnagrams_004/main.java
@@ -12,29 +12,51 @@ public class main {
 
 class Solution {
     public static List<List<String>> groupAnagrams(String[] strs) {
-        String[] sortedStr = new String[strs.length];
+        Map<String, List<String>> maps = new HashMap<>();
+
         for(int i=0; i< strs.length; i++){
             char[] word = strs[i].toCharArray();
             Arrays.sort(word);
-            sortedStr[i] = new String(word);
-        }
-        Map<String, List<String>> maps = new HashMap<>();
+            String sortedWord = new String(word);
 
-        int index=0;
-        for(String s : sortedStr){
-            if(maps.containsKey(s)){
-                List<String> list =maps.get(s);
-                list.add(strs[index++]);
-                maps.put(s, list);
-            }
-            else{
-                ArrayList<String> list = new ArrayList();
-                list.add(strs[index++]);
-                maps.put(s, list);
-            }
+            List<String> list;
+            if(maps.containsKey(sortedWord))
+                list=maps.get(sortedWord);
+            else
+                list = new ArrayList();
+
+            list.add(strs[i]);
+            maps.put(sortedWord, list);
+
         }
         return maps.values().stream()
                 .sorted(Comparator.comparingInt(m-> m.size()))
                 .toList();
     }
 }
+
+//String[] sortedStr = new String[strs.length];
+//        for(int i=0; i< strs.length; i++){
+//char[] word = strs[i].toCharArray();
+//            Arrays.sort(word);
+//sortedStr[i] = new String(word);
+//        }
+//Map<String, List<String>> maps = new HashMap<>();
+//
+//int index=0;
+//        for(String s : sortedStr){
+//        if(maps.containsKey(s)){
+//List<String> list =maps.get(s);
+//                list.add(strs[index++]);
+//                maps.put(s, list);
+//            }
+//                    else{
+//ArrayList<String> list = new ArrayList();
+//                list.add(strs[index++]);
+//                maps.put(s, list);
+//            }
+//                    }
+//                    return maps.values().stream()
+//                .sorted(Comparator.comparingInt(m-> m.size()))
+//        .toList();
+//    }
