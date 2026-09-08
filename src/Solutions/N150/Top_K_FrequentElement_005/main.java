ccabfe23ba2ea4e116d02eda10cee7ae0d5bca62
fevzierenn <fevzierencicek@gmail.com>
2026-09-08 17:54:36 +0300

    Top K Frequent Elements 005

---
+++ src/Solutions/N150/Top_K_FrequentElement_005/main.java
@@ -0,0 +1,35 @@
+package N150.Top_K_FrequentElement_005;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class main {
+    public static void main(String[] args) {
+        int[] list = Solution.topKFrequent(new int[]{1,1,1,2,2,3,2,3,2,3,1,2,2},2);
+        Arrays.stream(list).forEach(System.out::println);
+    }
+}
+
+class Solution {
+    public static int[] topKFrequent(int[] nums, int k) {
+        int[] topK = new int[k];
+        Map<Integer,Integer> maps = new HashMap<>();
+        for (Integer num : nums){
+            if(!maps.containsKey(num))
+                maps.put(num,1);
+            else
+                maps.put(num, maps.get(num)+1);
+        }
+        Object[] topKeys = maps.entrySet().stream()
+                .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
+                .limit(k)
+                .map(Map.Entry::getKey)
+                .toArray(Object[]::new);
+        for(int i=0; i<k;i++)
+            topK[i] = (int) topKeys[i];
+        return topK;
+    }
+}
